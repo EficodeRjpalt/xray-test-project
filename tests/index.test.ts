40f768dd1af25b43ab5281ebfbfd4f1bcf50a073
Rasmus Paltschik <rjpalt@gmail.com>
2024-01-15 13:39:43 +0200

    GIDP-2 tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,35 +1,25 @@
 import { addTwoNumbers, multiplyTwoNumbers, divideTwoNumbers, squareTwoNumbers } from '../src/index';
 
-/**
- * @tags GIDP-1
- */
-
-describe('addTwoNumbers', () => {
-    /**
-    * @tags GIDP-1
-    */
-  it('adds two numbers', () => {
+describe('GIDP-2', () => {
+it('adds two numbers', () => {
     expect(addTwoNumbers(1, 2)).toEqual(3);
-  });
+});
 });
 
-describe('multiplyTwoNumbers', () => {
-    /**
-    * @tags GIDP-3
-    */
-  it('multiplies two numbers', () => {
+describe('GIDP-2', () => {
+it('multiplies two numbers', () => {
     expect(multiplyTwoNumbers(1, 2)).toEqual(2);
-  });
+});
 });
 
-describe('divideTwoNumbers', () => {
-  it('divides two numbers', () => {
+describe('GIDP-2', () => {
+it('divides two numbers', () => {
     expect(divideTwoNumbers(1, 2)).toEqual(0.5);
-  });
+});
 });
 
-describe('squareTwoNumbers', () => {
-  it('squares two numbers', () => {
+describe('GIDP-2', () => {
+it('squares two numbers', () => {
     expect(squareTwoNumbers(2, 2)).toEqual(4);
-  });
+});
 });

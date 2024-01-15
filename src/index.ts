60adc229b1e79f4ccb01e5128a6bd9bd0cc8c8af
Rasmus Paltschik <rjpalt@gmail.com>
2024-01-15 11:16:30 +0200

    Add a failing etest

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const addTwoNumbers = (a: number, b: number) => a + b;
+export const addTwoNumbers = (a: number, b: number) => a + b +1;
 export const multiplyTwoNumbers = (a: number, b: number) => a * b;
 export const divideTwoNumbers = (a: number, b: number) => a / b;

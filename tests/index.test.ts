0fc542aa6f7c0dcc9e720134d70739efa7db57ba
Rasmus Paltschik <rjpalt@gmail.com>
2024-01-15 13:16:53 +0200

    Add tags to tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,22 @@
 import { addTwoNumbers, multiplyTwoNumbers, divideTwoNumbers, squareTwoNumbers } from '../src/index';
 
+/**
+ * @tags GIDP-1
+ */
+
 describe('addTwoNumbers', () => {
+    /**
+    * @tags GIDP-1
+    */
   it('adds two numbers', () => {
     expect(addTwoNumbers(1, 2)).toEqual(3);
   });
 });
 
 describe('multiplyTwoNumbers', () => {
+    /**
+    * @tags GIDP-3
+    */
   it('multiplies two numbers', () => {
     expect(multiplyTwoNumbers(1, 2)).toEqual(2);
   });
